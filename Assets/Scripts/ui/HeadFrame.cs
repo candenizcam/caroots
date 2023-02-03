322bd0e70626bdb56375cca5b0152473e722b573
candenizcam <e193777@metu.edu.tr>
2023-02-03 22:51:58 +0300

    jesus must die

---
+++ Assets/Scripts/ui/HeadFrame.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using DefaultNamespace.GameData;
+using Punity.ui;
+using UnityEngine.UIElements;
+
+namespace DefaultNamespace
+{
+    public class HeadFrame : VisualElement
+    {
+        public readonly string Id;
+        private PickableCharacters _charData;
+
+        public HeadFrame(string id, float width, float height)
+        {
+            Id = id;
+            _charData = DataBase.PickableCharactersArray.First(x => x.Id == id);
+
+            style.width = width;
+            style.height = height;
+            style.backgroundImage = QuickAccess.LoadSpriteBg("ui/pickframe");
+
+
+
+
+            var bc = new ButtonClickable(FrameClick);
+
+        }
+
+
+        private void FrameClick()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/ui/HeadPicker.cs
@@ -0,0 +1,18 @@
+﻿using DefaultNamespace.GameData;
+using UnityEngine.UIElements;
+
+namespace DefaultNamespace
+{
+    public class HeadPicker : VisualElement
+    {
+        public HeadPicker(LevelRecord thisLevel)
+        {
+            var r = 4;
+            var c = 3;
+            
+            
+            
+
+        }
+    }
+}
