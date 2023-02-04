4d94d76b2f6aa36ae75625b1f6451bcd2cd6fa3a
mandosfire <112665805+mandosfire@users.noreply.github.com>
2023-02-04 10:01:05 +0300

    ting tang vala vala bing bang

--- Assets/Scripts/GameData/DataBase.cs
+++ Assets/Scripts/GameData/DataBase.cs
@@ -4,8 +4,18 @@
     {
         public static PickableCharacters[] PickableCharactersArray = new  PickableCharacters[] 
         {
-            new ("test","testguy","frameImages/testguy")
-
+            new ("karahalka","karahalka","frameImages/testguy"),
+            new ("ogulcan","oğulcan","frameImages/testguy"),
+            new ("sümbül","sümbül","frameImages/testguy"),
+            new ("cabbar","cabbar","frameImages/testguy"),
+            new ("diken","diken","frameImages/testguy"),
+            new ("altıntekme","altıntekme","frameImages/testguy"),
+            new ("saban","şaban","frameImages/testguy"),
+            new ("ramazan","ramazan","frameImages/testguy"),
+            new ("abuzer","abuzer","frameImages/testguy"),
+            new ("recep","recep","frameImages/testguy"),
+            new ("tosun","tosun","frameImages/testguy"),
+            new ("nisa","nisa","frameImages/testguy"),
         };
         
         
